4f829ca7e9b803c2a00f258853324128df580cd4
KamgaBrayan <brayanarmel@gmail.com>
2026-01-16 10:03:36 +0100

    Condition for creating columns

--- src/main/resources/db/changelog/migration/03-add-user-photo.sql
+++ src/main/resources/db/changelog/migration/03-add-user-photo.sql
@@ -3,5 +3,5 @@
 --changeset brayan:03-add-user-photo-columns
 
 ALTER TABLE users 
-ADD COLUMN photo_id UUID,
-ADD COLUMN photo_uri VARCHAR(512);
+ADD COLUMN IF NOT EXISTS photo_id UUID,
+ADD COLUMN IF NOT EXISTS photo_uri VARCHAR(512);
