d286d6d794c3cd2ca83b610c90d4017aeec3903a
KamgaBrayan <brayanarmel@gmail.com>
2026-04-20 11:47:08 +0100

    Swagger Config in environnement variables

--- src/main/java/com/tramasys/auth/config/SwaggerConfig.java
+++ src/main/java/com/tramasys/auth/config/SwaggerConfig.java
@@ -1,27 +1,42 @@
 package com.tramasys.auth.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 @Configuration
-@OpenAPIDefinition(
-    info = @Info(title = "TraMaSys Auth API", version = "v1", description = "Service d'authentification centralisé"), 
-
-    servers = {
-        // Le premier serveur est celui par défaut dans l'UI Swagger
-        @Server(url = "https://traefikdev.yowyob.com/auth", description = "Serveur de Développement (via Proxy)"),
-        @Server(url = "/", description = "Serveur Local / Relatif")
-    }
+@SecurityScheme(
+    name = "bearerAuth", 
+    type = SecuritySchemeType.HTTP, 
+    bearerFormat = "JWT", 
+    scheme = "bearer"
 )
-@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SwaggerConfig {
+
+    // On injecte la valeur définie dans application.properties
+    @Value("${app.swagger.server-url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TraMaSys Auth API")
+                        .version("v1")
+                        .description("Service d'authentification centralisé"))
+                .servers(List.of(
+                        new Server().url(serverUrl).description("Serveur défini par environnement"),
+                        new Server().url("/").description("Serveur Local (Relatif)")
+                ));
+    }
 
     @Bean
     public GroupedOpenApi publicApi() {
